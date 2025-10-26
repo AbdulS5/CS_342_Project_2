aa2529c6ba26fefc11c8ef566dabbf5add993bf9
Hung Duy Nguyen <dnguy101@uic.edu>
2025-10-26 15:18:03 -0500

    Update GameScreen.java

--- src/main/java/GameScreen.java
+++ src/main/java/GameScreen.java
@@ -26,21 +26,33 @@ public class GameScreen {
         VBox gameLayout = new VBox(20, gameTitle);
         gameLayout.setAlignment(Pos.CENTER);
 
+       Label spotTitle = new Label("Pick number of spots");
+        GridPane chooseSpots = createButtonTable(1,10, 20);
+        chooseSpots.setAlignment(Pos.CENTER);
+        Label drawTitle = new Label("Pick number of drawings");
+        GridPane numDraws = createButtonTable(1,4, 20);
+        numDraws.setAlignment(Pos.CENTER);
+        VBox gameLayout_top = new VBox(10, drawTitle, numDraws);
+        VBox gameLayout_bottom = new VBox(10, spotTitle,chooseSpots);
+        gameLayout_top.setAlignment(Pos.CENTER);
+        gameLayout_bottom.setAlignment(Pos.CENTER);
+
+
+        VBox gameLayout =  new VBox(20, gameLayout_top, gameLayout_bottom);
+        gameLayout.setAlignment(Pos.CENTER);
         BorderPane gameRoot = new BorderPane();
         gameRoot.setTop(gameMenuBar);
-        gameRoot.setCenter(gameLayout);
-
-        GridPane table = BetCard.createButtonTable(10, 8);
+        gameRoot.setLeft(gameLayout);
+        GridPane table = createButtonTable(10,8, 30);
         gameRoot.setRight(table);
-
-        scene = new Scene(gameRoot, 800, 600);
-
+        gameScreen = new Scene(gameRoot, 1000, 600);
+        
         rulesItem2.setOnAction(e -> mainApp.showRules());
         oddsItem2.setOnAction(e -> mainApp.showOdds());
         backItem.setOnAction(e -> mainApp.showWelcomeScene());
     }
 
     public Scene getScene() {
-        return scene;
+        return gameScreen;
     }
 }
