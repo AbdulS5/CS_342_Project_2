0f7107b5804ccc953b3bd45bbaddb8dd4065a4c8
Abdul Samad <abdulsamad11112222@gmail.com>
2025-10-26 15:32:22 -0500

    Refactor GameScreen layout and menu items

--- src/main/java/GameScreen.java
+++ src/main/java/GameScreen.java
@@ -11,9 +11,10 @@ import javafx.scene.layout.VBox;
 
 public class GameScreen {
 
-    private Scene scene;
+    private Scene gameScreen;
 
     public GameScreen(JavaFXTemplate mainApp) {
+    	
         MenuBar gameMenuBar = new MenuBar();
         Menu gameMenu = new Menu("Menu");
         MenuItem rulesItem2 = new MenuItem("Rules of the Game");
@@ -21,30 +22,30 @@ public class GameScreen {
         MenuItem backItem = new MenuItem("Back to Menu");
         gameMenu.getItems().addAll(rulesItem2, oddsItem2, new SeparatorMenuItem(), backItem);
         gameMenuBar.getMenus().add(gameMenu);
+        
+        GridPane table = BetCard.createButtonTable(10, 8, 30);
 
-        Label gameTitle = new Label("KENO GAME SCREEN");
-        VBox gameLayout = new VBox(20, gameTitle);
-        gameLayout.setAlignment(Pos.CENTER);
-
-       Label spotTitle = new Label("Pick number of spots");
-        GridPane chooseSpots = createButtonTable(1,10, 20);
+        Label spotTitle = new Label("Pick number of spots");
+        GridPane chooseSpots = BetCard.createButtonTable(1,10, 20);
         chooseSpots.setAlignment(Pos.CENTER);
+        
         Label drawTitle = new Label("Pick number of drawings");
-        GridPane numDraws = createButtonTable(1,4, 20);
+        GridPane numDraws = BetCard.createButtonTable(1,4, 20);
         numDraws.setAlignment(Pos.CENTER);
+        
         VBox gameLayout_top = new VBox(10, drawTitle, numDraws);
-        VBox gameLayout_bottom = new VBox(10, spotTitle,chooseSpots);
+        VBox gameLayout_bottom = new VBox(10, spotTitle, chooseSpots);
         gameLayout_top.setAlignment(Pos.CENTER);
         gameLayout_bottom.setAlignment(Pos.CENTER);
 
-
-        VBox gameLayout =  new VBox(20, gameLayout_top, gameLayout_bottom);
+        VBox gameLayout = new VBox(20, gameLayout_top, gameLayout_bottom);
         gameLayout.setAlignment(Pos.CENTER);
+        
         BorderPane gameRoot = new BorderPane();
         gameRoot.setTop(gameMenuBar);
         gameRoot.setLeft(gameLayout);
-        GridPane table = createButtonTable(10,8, 30);
         gameRoot.setRight(table);
+        
         gameScreen = new Scene(gameRoot, 1000, 600);
         
         rulesItem2.setOnAction(e -> mainApp.showRules());
