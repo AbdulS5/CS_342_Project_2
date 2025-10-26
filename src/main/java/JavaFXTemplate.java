315c8d527654f57a410a0720675a5844aa973321
Abdul Samad <abdulsamad11112222@gmail.com>
2025-10-26 13:03:01 -0500

    Refactor JavaFXTemplate for Keno Lottery Game

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -1,117 +1,69 @@
-import javafx.animation.FadeTransition;
-import javafx.animation.PauseTransition;
-import javafx.animation.RotateTransition;
-import javafx.animation.SequentialTransition;
 import javafx.application.Application;
-
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Tooltip;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-
+import javafx.stage.Stage;
+import javafx.scene.control.Alert;
 
 public class JavaFXTemplate extends Application {
 
-    Button b1;
-    TextField t1;
-    VBox vbox;
-    HBox bbox;
-    EventHandler<ActionEvent> myhandler;
-    MenuBar menu;
-    BorderPane root;
+    private Stage primaryStage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	//feel free to remove the starter code from this method
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
-		
-		 //Rectangle rect = new Rectangle (100, 40, 100, 100);
-	     //rect.setArcHeight(50);
-	     //rect.setArcWidth(50);
-	     //rect.setFill(Color.VIOLET);
+    public void start(Stage stage) {
+        primaryStage = stage;
+        stage.setTitle("Keno Lottery Game");
 
-	     //RotateTransition rt = new RotateTransition(Duration.millis(5000), rect);
-	     //rt.setByAngle(270);
-	     //rt.setCycleCount(4);
-	     //rt.setAutoReverse(true);
-	     //SequentialTransition seqTransition = new SequentialTransition (
-	         //new PauseTransition(Duration.millis(500)),
-	         //rt
-	     //);
-	     //seqTransition.play();
-	     
-	     //FadeTransition ft = new FadeTransition(Duration.millis(5000), rect);
-	     //ft.setFromValue(1.0);
-	     //ft.setToValue(0.3);
-	     //ft.setCycleCount(4);
-	     //ft.setAutoReverse(true);
+        MenuBar menuBar = new MenuBar();
+        Menu menu = new Menu("Menu");
+        MenuItem rulesItem = new MenuItem("Rules of the Game");
+        MenuItem oddsItem = new MenuItem("Odds of Winning");
+        MenuItem exitItem = new MenuItem("Exit Game");
+        menu.getItems().addAll(rulesItem, oddsItem, new SeparatorMenuItem(), exitItem);
+        menuBar.getMenus().add(menu);
 
-	     //ft.play();
-	     //BorderPane root = new BorderPane();
-	     //root.setCenter(rect);
+        Label title = new Label("Welcome to KENO");
+        Button startBtn = new Button("Start Game");
+        VBox centerBox = new VBox(20, title, startBtn);
+        centerBox.setAlignment(Pos.CENTER);
 
-        b1 = new Button("Start");
-        b1.setMinWidth(100);
+        BorderPane root = new BorderPane();
+        root.setTop(menuBar);
+        root.setCenter(centerBox);
 
-        t1 = new TextField();
+        startBtn.setOnAction(e -> System.out.println("Start Game clicked"));
+        rulesItem.setOnAction(e -> showRules());
+        oddsItem.setOnAction(e -> showOdds());
+        exitItem.setOnAction(e -> primaryStage.close());
 
-        Region r1 = new Region();
-        r1.setMinWidth(30);
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-        menu = new MenuBar();
-        Menu mOne = new Menu("Menu");
+    private void showRules() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Rules of Keno");
+        alert.setHeaderText("Keno Lottery Game Rules");
+        alert.setContentText("Pick 1–10 numbers from 1–80.\nTwenty numbers will be drawn.\nYour winnings depend on how many you match.");
+        alert.showAndWait();
+    }
 
-        bbox = new HBox(b1,r1);
-        vbox = new VBox(menu, bbox, t1);
-        root = new BorderPane();
-        root.setCenter(vbox);
-        //root.setStyle("-fx-padding: 20;");
-
-        //b2.setOnAction(new EventHandler<ActionEvent>() {
-        //    public void handle(ActionEvent e) {
-        //        t1.clear();
-        //    }
-        //});
-
-        b1.setOnAction(e->t1.setText("Testing"));
-
-        MenuItem item = new MenuItem("click me");
-        item.setOnAction(e->t1.setText("item was clicked"));
-        mOne.getItems().add(item);
-
-        menu.getMenus().addAll(mOne);
-
-        Scene scene = new Scene(root, 700,700);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		
-				
-		
-	}
-
+    private void showOdds() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Odds of Winning");
+        alert.setHeaderText("Keno Odds Table");
+        alert.setContentText("hello");
+        alert.showAndWait();
+    }
 }
